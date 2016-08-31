7c95bd26666dab2f4ad0ce79b5fb30b65803dd9a
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-08-31 22:47:29 +0200

    ajout du domain, scale, axes pour d3.js

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -5,7 +5,9 @@ angular.module('cageApp').controller('mainCtrl', function($scope, movies){
         for(var pageEnCours = 1; pageEnCours <= nbPages; pageEnCours++){
             movies.query({page: pageEnCours}, function(filmsParPage){
                 for(film in filmsParPage.results){
+                    if(filmsParPage.results[film].release_date != ''){
                         filmographie.push(filmsParPage.results[film]);
+                    }
                 }
                 $scope.filmographie = filmographie;
             });
--- app/scripts/directives/graphique.js
+++ app/scripts/directives/graphique.js
@@ -1,25 +1,48 @@
-angular.module('cageApp').directive('graphique', function($timeout){
+angular.module('cageApp').directive('graphique', function(){
     return {
             restrict: 'E',
             scope:{
                 films: '='
             },
             templateUrl: 'templates/graphique.html',
-            link: function(scope, element, attrs) {
-                var films = scope.films;
-                var svg = d3.select(element[0]).append('svg').attr('width', 500).attr('height', 500);
-                svg.selectAll('circle')
-                    .data(films)
-                    .enter()
-                    .append('circle')
-                    .attr('cy', function(d){
-                        return d.vote_average;
-                    })
-                    .attr('cx', function(d){
-                        var annee = d.release_date;
-                        return annee.substring(0, 4);
-                    })
-                    .attr('r', 10);
+            link: function(scope, element, attrs){
+                films = scope.films,
+
+                // time parsing's function
+                parseDate = d3.time.format('%y-%b-%d').parse,
+
+                // Set canvas' dimensions
+                width = 800,
+                height= 500,
+
+                // Set the range of the canvas
+                x = d3.time.scale().range([0, width]),
+                y = d3.scale.linear().range([height, 0]);
+
+                // Parsing each release_date movie
+                films.forEach((d) => {
+                    d.release_date = parseDate(d.release_date);
+                });
+
+                // Scale the range of datas
+                x.domain(d3.extent(films, (d) => { return d.release_date; }));
+                y.domain([0, d3.max(films,(d) => { return d.vote_average; })]);
+
+                var xAxis = d3.svg.axis().scale(x)
+                    .orient('bottom').ticks(5);
+
+                var yAxis = d3.svg.axis().scale(y)
+                    .orient("left").ticks(10);
+
+                var valueLine = d3.svg.line()
+                    .x((d) => { return x(d.release_date); })
+                    .y((d) => { return y(d.vote_average); });
+
+                var svg = d3.select('body')
+                            .append('svg')
+                                .attr('width', width)
+                                .attr('height', height);
+
             }
     }
 })
