843aca6b66df997c6db572bab472c9fdfb61b34f
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-09-01 22:55:41 +0200

    Suppression des films avec note =0 ou =10

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -5,7 +5,7 @@ angular.module('cageApp').controller('mainCtrl', function($scope, movies){
         for(var pageEnCours = 1; pageEnCours <= nbPages; pageEnCours++){
             movies.query({page: pageEnCours}, function(filmsParPage){
                 for(film in filmsParPage.results){
-                    if(filmsParPage.results[film].release_date != ''){
+                    if(filmsParPage.results[film].release_date != '' && filmsParPage.results[film].vote_average != 10 && filmsParPage.results[film].vote_average != 0){
                         filmographie.push(filmsParPage.results[film]);
                     }
                 }
