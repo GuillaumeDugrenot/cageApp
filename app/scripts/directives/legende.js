93ce84d1bfa7261c9f466966f49763b17e7313e2
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-09-02 12:04:30 +0200

    ajout de la directive legende

---
+++ app/scripts/directives/legende.js
@@ -0,0 +1,14 @@
+angular.module('cageApp').directive('legende', function(actorInfos) {
+    return {
+        restrict: 'E',
+        templateAsUrl: 'templates/legende.html',
+        replace: true,
+        link: function(scope, element, attrs) {
+            actorInfos.genres.query(function(results) {
+                var genres = results.genres;
+                console.log(genres);
+            });
+
+        }
+    }
+})
