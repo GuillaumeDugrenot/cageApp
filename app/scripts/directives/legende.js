729332e91354839eca3e79f5bb9cd87b7da147c8
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-09-03 21:43:37 +0200

    ''

--- app/scripts/directives/legende.js
+++ app/scripts/directives/legende.js
@@ -3,11 +3,5 @@ angular.module('cageApp').directive('legende', function(actorInfos) {
         restrict: 'E',
         templateAsUrl: 'templates/legende.html',
         replace: true,
-        link: function(scope, element, attrs) {
-            actorInfos.genres.query(function(results) {
-                var genres = results.genres;
-            });
-
-        }
     }
 })
