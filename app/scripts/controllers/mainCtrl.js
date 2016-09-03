b93249eadde11a00930ec434c01e890d83a1f2d2
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-09-03 21:39:04 +0200

    Ajout variable genres des différents films

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -1,16 +1,29 @@
 angular.module('cageApp').controller('mainCtrl', function($scope, actorInfos){
+    // Recherches des films avec le provider actorInfos
     actorInfos.movies.query(function(films) {
-        var nbPages = films.total_pages;
+        var nbPages      = films.total_pages;
+        var genres       = [];
         var filmographie = [];
         for(var pageEnCours = 1; pageEnCours <= nbPages; pageEnCours++){
             actorInfos.movies.query({page: pageEnCours}, function(filmsParPage){
                 for(film in filmsParPage.results){
-                    if(filmsParPage.results[film].release_date != '' && filmsParPage.results[film].vote_average != 10 && filmsParPage.results[film].vote_average != 0){
+                    if(filmsParPage.results[film].release_date != ''
+                    && filmsParPage.results[film].vote_average != 10
+                    && filmsParPage.results[film].vote_average != 0
+                    && filmsParPage.results[film].genre_ids.length != 0){
+                        if(genres.indexOf(filmsParPage.results[film].genre_ids[0]) === -1){
+                            genres.push(filmsParPage.results[film].genre_ids[0]);
+                        }
                         filmographie.push(filmsParPage.results[film]);
                     }
                 }
-                $scope.filmographie = filmographie;
             });
         }
+        $scope.filmographie = filmographie;
+    });
+
+    //Recherche des genres
+    actorInfos.genres.query(function(results){
+        var genres = results.genres;
     });
 });
