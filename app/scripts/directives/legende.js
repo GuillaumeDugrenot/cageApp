3ae30d38a64bab308a2af58b7ae63fefcf98250c
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-09-05 18:46:23 +0200

    Modification fonction de recherche de genre cinématographique

--- app/scripts/directives/legende.js
+++ app/scripts/directives/legende.js
@@ -1,31 +1,30 @@
 angular.module('cageApp').directive('legende', function($q, actorInfos) {
     return {
         restrict: 'E',
-        templateAsUrl: 'templates/legende.html',
+        // templateAsUrl: 'scripts/templates/legende.html',
+        templateUrl: 'templates/legende.html',
         replace: true,
         link: function(scope, element, attrs) {
             var deferred = $q.defer();
             var promiseGenres = deferred.promise;
+
             // Appel à l'API retournant dans "promise" tous les genres cinématographique de la base.
             actorInfos.genres.query(function(results){
                 deferred.resolve(results.genres);
             });
 
             promiseGenres.then(function (genres) {
-                for(var i = 0; i < scope.genresFilmographie.length; i++){
-                    scope.genresFilmographie[i] = trouveGenreCorrespondant(i, genres);
-                }
-                console.log(scope.genresFilmographie);
+                scope.genresFilmographie.map(function (genre, index, tableau){
+                    for(var j = 0, len = genres.length; j < len; j++){
+                        if(genres[j].id === genre){
+                            tableau[index] = genres[j].name;
+                            return tableau[index];
+                        }
+                    }
+                });
             });
 
-            // @idGenre: correspond à l'id d'un genre d'un genre cinématographique présent dans la filmographie de l'acteur
-              function trouveGenreCorrespondant(i, genres){
-                for(var j = 0, len = genres.length; j < len; j++){
-                    if(genres[j].id === scope.genresFilmographie[i]){
-                        return  genres[j].name;
-                    }
-                }
-            };
+
         }
     }
 });
