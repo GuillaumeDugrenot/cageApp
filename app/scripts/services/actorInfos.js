7d30fd9dcdaca827554750c3fbf27bd3cd40f859
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-09-24 18:11:28 +0200

    Ajout paramètre permettant d'avoir les résumés en français

--- app/scripts/services/actorInfos.js
+++ app/scripts/services/actorInfos.js
@@ -1,16 +1,20 @@
 angular.module('cageApp').provider('actorInfos', function() {
     var apiKey = '';
     var actor = '';
+    var language = '';
     this.setApiKey = function(cle) {
         apiKey = cle;
     };
     this.setActor = function(idActor) {
         actor = idActor;
     };
+    this.setLangage = function(langue) {
+        language = langue;
+    }
     this.$get = function($resource) {
         return {
             genres: $resource('http://api.themoviedb.org/3/genre/movie/list', {}, {'query': {method: 'GET', params: {api_key: apiKey}}}),
-            movies: $resource('http://api.themoviedb.org/3/discover/movie?sort_by=release_date.asc', {}, {'query': {method: 'GET', params: {api_key: apiKey, with_cast: actor}}})
+            movies: $resource('http://api.themoviedb.org/3/discover/movie?sort_by=release_date.asc', {}, {'query': {method: 'GET', params: {api_key: apiKey, with_cast: actor, language: language}}})
 
         }
     }
