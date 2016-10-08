536b10151a52aba7eaf5d8b18d4b251aa2c2a62a
Guillaume DUGRENOT <Guillaume DUGRENOT>
2016-10-08 15:55:02 +0200

    Modification commentaire fonction setActive()

--- app/scripts/controllers/mainCtrl.js
+++ app/scripts/controllers/mainCtrl.js
@@ -27,7 +27,7 @@ angular.module('cageApp').controller('mainCtrl', function($scope, $q, actorInfos
         $scope.genresFilmographie = genres;
     });
 
-    // @genreName = représente le genre choisi par l'utilisateur au moment du click sur la légende des genres.
+    // @genre = représente le genre choisi par l'utilisateur au moment du click sur la légende des genres.
     $scope.setActive = function(genre) {
         $scope.activeGenre = genre;
         $scope.isFiltering = true;
